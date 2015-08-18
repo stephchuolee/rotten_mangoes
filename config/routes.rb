19a54463cd2db43ad70a06d2a3a4c1792f421979
schlorantics <schuolee@gmail.com>
2015-08-18 18:12:10 +0000

    17. routing root to movies#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 RottenMangoes::Application.routes.draw do
   get "reviews/new"
   get "reviews/create"
+
   resources :movies do 
     resources :reviews, only: [:new, :create]
   end 
+
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
+  root to: 'movies#index'
+  
 end

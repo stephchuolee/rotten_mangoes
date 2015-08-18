8bcf50adc9705504ff296ceae3026d84c50d79ee
schlorantics <schuolee@gmail.com>
2015-08-18 14:28:03 +0000

    6. displaying validations errors in movies form

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,28 @@
 class Movie < ActiveRecord::Base
 
-  validates :title, :director, :description, :poster_image_url, :release_date, presence: true 
-  validates :runtime_in_minutes, numericality: { only_integer: true }
+  # validates :title, :description, :poster_image_url, :release_date, presence: true 
+  # validates :director, presence: true
+  # validates :runtime_in_minutes, numericality: { only_integer: true }
+  # validate :release_date_is_in_the_future
+
+  validates :title,
+    presence: true
+
+  validates :director,
+    presence: true
+
+  validates :runtime_in_minutes,
+    numericality: { only_integer: true }
+
+  validates :description,
+    presence: true
+
+  validates :poster_image_url,
+    presence: true
+
+  validates :release_date,
+    presence: true
+
   validate :release_date_is_in_the_future
 
   protected 

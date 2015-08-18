70e3ce99f3d322de9d71a32e2e070732d995092a
schlorantics <schuolee@gmail.com>
2015-08-18 17:58:09 +0000

    13. generating reviews controller and reviews/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 RottenMangoes::Application.routes.draw do
-  resources :movies
+  get "reviews/new"
+  get "reviews/create"
+  resources :movies do 
+    resources :reviews, only: [:new, :create]
+  end 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
